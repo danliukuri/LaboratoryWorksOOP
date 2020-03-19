c4cc56aa5dbcd02d9d1f53b03296b3b5eebb0596
yura daniliyk <yuradaniliyk@gmail.com>
2020-03-19 10:59:17 +0200

    Removed unnecessary implementation

--- Laboratory work 3 Cs/Program.cs
+++ Laboratory work 3 Cs/Program.cs
@@ -6,27 +6,11 @@ namespace Laboratory_work_3_Cs
     {
         static void Main(string[] args)
         {
-            ArrChar arr = new ArrChar();
-            ArrChar arr2 = new ArrChar();
-
-            arr2[1] = arr[1];
-
-            arr[1] = 'f';
-            arr[0] = 'a';
-            arr[1] = 'E';
-
-            arr2[0] = arr[2];
-
-            int numberOfVowels = arr.NumberOfVowels;
-        }
-
-        static void MainTwoRealization(string[] args)
-        {
-            ArrCharTwoRealization a = new ArrCharTwoRealization(2);
+            ArrChar a = new ArrChar(2);
             a[-1] = 'f';
             a[1] = 'f';
             a[0] = 'a';
-            ArrCharTwoRealization c = new ArrCharTwoRealization(1);
+            ArrChar c = new ArrChar(1);
             c[0] = a[1];
 
             int b = a.NumberOfVowels;
